bb6f439ab17db8d0220aeb85a86835f5e35d2f63
Gabriel Reis <gabrielreisctt1@gmail.com>
2025-05-30 15:35:42 -0300

    fix gitignore

--- src/routes/testRoutes.ts
+++ src/routes/testRoutes.ts
@@ -1,4 +1,5 @@
 import { FastifyReply, FastifyRequest, FastifyInstance } from "fastify";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 class HomeController {
   public async index(req: FastifyRequest, res: FastifyReply): Promise<void> {
@@ -12,3 +13,8 @@ const home = new HomeController();
 export async function testRoutes(app: FastifyInstance) {
   app.get("/", home.index);
 }
+
+// fastify.get("/dashboard", {
+//   preHandler: [authMiddleware],
+//   handler: dashboardController.index
+// });
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { userController } from "../controllers/UserController";
+import { authMiddleware } from "../middlewares/authMiddleware";
 
 export async function userRoutes(app: FastifyInstance) {
   app.post("/", userController.create);
