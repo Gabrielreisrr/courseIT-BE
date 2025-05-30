9b51c74f9391ca5a0a97e857612e31a382630a13
Reis <134563219+Gabrielreisrr@users.noreply.github.com>
2025-05-29 21:46:41 -0300

    Delete dist directory

--- dist/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userRoutes = userRoutes;
-const UserController_1 = require("../controllers/UserController");
-async function userRoutes(app) {
-    app.post("/", UserController_1.userController.create);
-    app.post("/login", UserController_1.userController.login);
-}
